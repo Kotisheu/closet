06015e81694d2fdc593c8ff2e50f9e63e052762b
Fitzy97 <seanfitzgerald321@gmail.com>
2013-12-11 14:44:04 -0500

    Update CandidatePool.java

--- CandidatePool/CandidatePool.java
+++ CandidatePool/CandidatePool.java
@@ -1,33 +1,32 @@
 /*=====================================
   class CandidatePool -- 
 
-  Each CandidatePool object contains the following
-  instance variables that you may need to use:
-       
-  pool
-      An ArrayList of only Candidate objects
+
+Each CandidatePool object contains the following
+instance variables that you may need to use:
+pool
+An ArrayList of only Candidate objects
 
 
-  You will need to write the following methods:
-   
-  getCandidatesForPosition( String pos )
-      Returns a candidate-only ArrayList containing all
-      the Candidates in pool that have position pos.
+You will need to write the following methods:
+getCandidatesForPosition( String pos )
+Returns a candidate-only ArrayList containing all
+the Candidates in pool that have position pos.
 
-  getBestCandidate( String pos )
-      Returns the Candidte in pool that matches position
-      pos with the highest score.
-      If there are no candidates for the given position, 
-      returns null.
+getBestCandidate( String pos )
+Returns the Candidte in pool that matches position
+pos with the highest score.
+If there are no candidates for the given position,
+returns null.
 
-  removeCandidatesForPosition( String pos )
-      Removes all the Candidates in the pool that match
-      position pos.
-      Returns number of candidates removed.
-  =====================================*/
+removeCandidatesForPosition( String pos )
+Removes all the Candidates in the pool that match
+position pos.
+Returns number of candidates removed.
+=====================================*/
 
 //We usually leave this out because Java does this automatically:
-import java.io.*; 
+import java.io.*;
 //...but this is not automatically performed (util pkg contains ArrayList):
 import java.util.*;
 
@@ -39,33 +38,44 @@ public class CandidatePool {
     ArrayList<Candidate> pool;
     
     public CandidatePool() {
-	pool = new ArrayList<Candidate>();
+        pool = new ArrayList<Candidate>();
     }
 
     public void addCandidate( Candidate c ) {
-	pool.add(c);
+        pool.add(c);
     }
 
     public String toString() {
-	return pool.toString();
-    }    
+        return pool.toString();
+    }
     //=================================
 
 
 
     //YOUR WORK BELOW:
     public ArrayList<Candidate> getCandidatesForPosition( String pos ) {
-	//*** YOUR IMPLEMENTATION HERE ***
+	ArrayList temp = new ArrayList();
+	for (int i = 0; i < pool.size(); i++) {
+	    if (pool.get(i).getPosition() == pos)
+		temp.add(pool.get(i));
+	}
+	return temp;
     }
 
 
-    public Candidate getBestCandidate( String pos ) { 
-	//*** YOUR IMPLEMENTATION HERE ***
+    public Candidate getBestCandidate( String pos ) {
+        ArrayList temp = getCandidatesForPosition( pos );
+	Candidate joe = new Candidate(0, pos, 0);
+	for (int i = 0; i < temp.size(); i++) {
+	    if (temp.get(i).getInterviewScore() > joe.getInterviewScore())
+		joe = temp.get(i);
+	}
+	return joe;
     }
 
 
     public int removeCandidatesForPosition( String pos ) {
-	//*** YOUR IMPLEMENTATION HERE ***
+        
     }
 
 
